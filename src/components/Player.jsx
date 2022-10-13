24552d26017bee1189ebf3d9025515d96634c83d
ConanGH-S <blandon0207s@gmail.com>
2022-10-13 18:29:00 -0500

    First component almost done

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,10 +5,10 @@ const Player = () => {
 		<main>
 			<form action="game.jsx">
 				<label htmlFor="player1">Digite el nombre del primer jugador</label>
-				<input type="text" name="player1" id="player1" />
+				<input type="text" name="player1" placeholder='Digite los datos aquí' id="player1" />
 				<label htmlFor="player2">Digite el nombre del segundo jugador</label>
-				<input type="text" name="player2" id="player2" />
-				<button>Ingresar</button>
+				<input type="text" name="player2" placeholder='Digite los datos aquí' id="player2" />
+				<button>Enviar</button>
 			</form>
 		</main>
 	);
